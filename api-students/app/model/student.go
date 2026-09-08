95dabe9a6340023b54f22f87b15564027d7a4e85
Fi-Rynix <rafiihyaazzaky@gmail.com>
2026-09-08 21:30:03 +0700

    feat: pool postgre & model

--- api-students/app/model/student.go
+++ api-students/app/model/student.go
@@ -1,46 +1,46 @@
-package main
+package model
 
 type Student struct {
 	ID       int
-	NIM			 int
+	NIM      int
 	Name     string
 	Grade    float64
 	IsActive bool
 }
 
 type CreateStudentRequest struct {
-	NIM			 int
+	NIM      int
 	Name     string
 	Grade    float64
 	IsActive bool
 }
 
 type UpdateStudentRequest struct {
-	NIM			 int
+	NIM      int
 	Name     string
 	Grade    float64
 	IsActive bool
 }
 
 type PatchStudentRequest struct {
-	NIM			 *int     `json:"nim,omitempty"`
-	Name     *string	`json:"name,omitempty"`
-	Grade    *float64	`json:"grade,omitempty"`
-	IsActive *bool	  `json:"is_active,omitempty"`
+	NIM      *int     `json:"nim,omitempty"`
+	Name     *string  `json:"name,omitempty"`
+	Grade    *float64 `json:"grade,omitempty"`
+	IsActive *bool    `json:"is_active,omitempty"`
 }
 
 type WebResponse struct {
 	Success bool
 	Message string
 	Data    any
-	Meta		*Meta
+	Meta    *Meta
 	Errors  any
 }
 
 type Meta struct {
 	Page       int
 	Limit      int
-	Total  		 int
+	Total      int
 	TotalPages int
 }
 
@@ -51,4 +51,8 @@ type ListQuery struct {
 	Sort     string
 	Order    string
 	IsActive *bool
+}
+
+func (q ListQuery) Offset() int {
+	return (q.Page - 1) * q.Limit
 }
